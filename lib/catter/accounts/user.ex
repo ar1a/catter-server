d7e4a5836f78b705c0b9996ce7879ad2e7582cb8
Aria <aria@ar1as.space>
2018-12-05 17:33:03 +1100

    Add proper error handling to mutations and queries

--- lib/catter/accounts/user.ex
+++ lib/catter/accounts/user.ex
@@ -2,13 +2,13 @@ defmodule Catter.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "users" do
-    field :email, :string
-    field :password_hash, :string
-    field :username, :string
+    field(:email, :string)
+    field(:password, :string, virtual: true)
+    field(:password_hash, :string)
+    field(:username, :string)
 
     timestamps()
   end
@@ -16,9 +16,24 @@ defmodule Catter.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :password_hash, :email])
-    |> validate_required([:username, :password_hash, :email])
+    |> cast(attrs, [:username, :password, :email])
+    |> validate_required([:username, :password])
+    |> validate_length(:username, min: 3, max: 20)
+    |> validate_length(:password, min: 8, max: 150)
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase/1)
     |> unique_constraint(:username)
     |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Argon2.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
--- lib/catter_web/schema.ex
+++ lib/catter_web/schema.ex
@@ -2,16 +2,18 @@ defmodule CatterWeb.Schema do
   use Absinthe.Schema
 
   query do
-    # FIXME: doesn't error properly if provided with a malformed UUID
-    field :mew, non_null(:mew) do
+    field :mew, :mew do
       arg(:id, non_null(:id))
 
       resolve(fn _, %{id: id}, _ ->
-        with mew <- Catter.Posts.get_mew(id) do
-          {:ok, mew}
-        else
-          _ -> {:error, "Cannot be found"}
-        end
+        mew =
+          try do
+            Catter.Posts.get_mew(id)
+          rescue
+            _ -> nil
+          end
+
+        {:ok, mew}
       end)
     end
 
@@ -20,8 +22,37 @@ defmodule CatterWeb.Schema do
     end
   end
 
+  mutation do
+    field :register, :user do
+      arg(:username, non_null(:string))
+      arg(:email, :string)
+      arg(:password, non_null(:string))
+
+      resolve(fn _, args, _ ->
+        case Catter.Accounts.create_user(args) do
+          {:error, changeset} ->
+            {:error, message: "Could not create user", details: error_details(changeset)}
+
+          success ->
+            success
+        end
+      end)
+    end
+  end
+
   object :mew do
     field(:id, non_null(:id))
     field(:content, non_null(:string))
+  end
+
+  object :user do
+    field(:id, non_null(:id))
+    field(:username, non_null(:id))
+    field(:email, :id)
+  end
+
+  def error_details(changeset) do
+    changeset
+    |> Ecto.Changeset.traverse_errors(fn {msg, _} -> msg end)
   end
 end
